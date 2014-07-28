5245cb85e10138f5307e6052b3d4125765bfb3c3
afazel <azar.fazel@gmail.com>
2014-07-28 16:12:15 -0700

    fix small bugs

--- links/azarf/Pro_Enh_Dya_Correlations.R
+++ links/azarf/Pro_Enh_Dya_Correlations.R
@@ -15,38 +15,22 @@ Dyadics_H3k4me1<-read.table("/srv/gsfs0/projects/kundaje/users/azarf/roadmap/sig
 Promoters<-read.table("/srv/gsfs0/projects/kundaje/users/azarf/roadmap/signal/Promoter.txt", check.names=FALSE, header=T)
 Enhancers<-read.table("/srv/gsfs0/projects/kundaje/users/azarf/roadmap/signal/Enhancer.txt", check.names=FALSE, header=T)
 
-ERow_name<-matrix( ,nrow=1,ncol=240)
-PRow_name<-matrix( ,nrow=1,ncol=85)
-PCol_name<-matrix( ,nrow=1,ncol=85)
-DCol_name<-matrix( ,nrow=1,ncol=193)
-
-index<-1
-for(i in 1:240){
-    name<-paste("cluster_",i,sep="")
-    ERow_name[1,index]<-name
-    if( i<=85){
-       PCol_name[1,index]<-name
-       PRow_name[1,index]<-name
-    }
-    if(i<=193){
-      DCol_name[1,index]<-name
-    }
-    index<-index+1
-}
 
 
-Corr_Matrix<-matrix( ,nrow=240,ncol=85)
-rownames(Corr_Matrix)<-ERow_name[1, ]
-colnames(Corr_Matrix)<-PCol_name[1, ]
+
+Corr_Matrix<-matrix( ,nrow=nrow(Enhancers),ncol=nrow(Promoters))
+rownames(Corr_Matrix)<-rownames(Enhancers)
+colnames(Corr_Matrix)<-rownames(Promoters)
 
 Output<-ComputeCorrealtion(Enhancers, Promoters, Corr_Matrix)
 write.table(Output,file="Promoter_Enhancer_Corr.txt",sep="\t")
 
 print("Enhacer_Promoter complete")
 
-Corr_Matrix<-matrix( ,nrow=240,ncol=193)
-rownames(Corr_Matrix)<-ERow_name[1, ]
-colnames(Corr_Matrix)<-DCol_name[1, ]
+Corr_Matrix<-matrix( ,nrow=nrow(Enhancers),ncol=nrow(Dyadics_H3k4me1))
+rownames(Corr_Matrix)<-rownames(Enhancers)
+colnames(Corr_Matrix)<-rownames(Dyadics_H3k4me1)
+
 Output<-ComputeCorrealtion(Enhancers, Dyadics_H3k4me1, Corr_Matrix)
 write.table(Output,file="Enhancer_Dyadics_H3k4me1_Corr.txt",sep="\t")
 print("Enhacer_H3k4me1 complete") 
@@ -55,9 +39,9 @@ Output<-ComputeCorrealtion(Enhancers, Dyadics_H3k4me3, Corr_Matrix)
 write.table(Output,file="Enhancer_Dyadics_H3k4me3_Corr.txt",sep="\t")
 print("Enhacer_H3k4me3 complete") 
 
-Corr_Matrix<-matrix( ,nrow=85,ncol=193)
-rownames(Corr_Matrix)<-PRow_name[1, ]
-colnames(Corr_Matrix)<-DCol_name[1, ]
+Corr_Matrix<-matrix( ,nrow=nrow*Promoters),ncol=nrow(Dyadics_H3k4me1))
+rownames(Corr_Matrix)<-rownames(Promoters)
+colnames(Corr_Matrix)<-rownames(Dyadics_H3k4me1)
 
 Output<-ComputeCorrealtion(Promoters, Dyadics_H3k4me1, Corr_Matrix)
 write.table(Output,file="Promoter_Dyadics_H3k4me1_Corr.txt",sep="\t")
