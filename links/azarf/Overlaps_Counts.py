18e6942ff6cafcad2ee4e6a3925632c722024c21
afazel <azar.fazel@gmail.com>
2014-08-26 14:48:37 -0700

    Update Overlaps_Counts.py

--- links/azarf/Overlaps_Counts.py
+++ links/azarf/Overlaps_Counts.py
@@ -2,12 +2,26 @@ import ntpath
 import os
 import glob
 
-files=glob.glob("/home/azarf/permutations/Overlaps/*.bed")
-OutFile="/home/azarf/permutations/Overlaps/Counts/Overlaps_Counts.txt"
+bedFiles=glob.glob("/home/azarf/permutations/Overlaps/*.bed")
+OutFile="/home/azarf/permutations/Overlaps/counts/Overlap_Counts.txt"
 
+with open("exp_comp_overlap.tab", 'r') as file:
+           fname=os.path.basename("exp_comp_overlap.tab")
+           count=0
+           lines = [line.strip() for line in file]
+           for l in lines:
+               if l.split('\t')[3].strip()==l.split('\t')[7].strip():
+                  count=count+1
 with open(OutFile, 'w') as out:
-     for f in files:
+     out.write("{0}\t{1}\n".format(fname,count))
+
+with open(OutFile, 'a') as out:
+     for f in bedFiles:
         fname=os.path.basename(f)
-        num_lines = sum(1 for line in open(f))
-        out.write("{0}\t{1}\n".format(fname,num_lines))
-           
+        count=0
+        with open(f, 'r') as file:
+           lines = [line.strip() for line in file]
+           for l in lines:
+               if l.split('\t')[3].strip()==l.split('\t')[7].strip():
+                  count=count+1
+        out.write("{0}\t{1}\n".format(fname,count))
